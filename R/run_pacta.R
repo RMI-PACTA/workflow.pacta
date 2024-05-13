0ffcd81c17002a06be02fe9c31657ec607593bdb
Alex Axthelm <aaxthelm@RMI.org>
2024-05-13 16:26:28 +0200

    Add minimal loggin to run_pacta

--- R/run_pacta.R
+++ R/run_pacta.R
@@ -1,6 +1,8 @@
 run_pacta <- function(
   cfg_path = commandArgs(trailingOnly = TRUE)
 ) {
+  log_info("Running PACTA")
   run_audit(cfg_path)
   run_analysis(cfg_path)
+  log_info("PACTA run complete.")
 }
