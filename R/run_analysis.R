39bdd4dd5f67916375796680c185d7a8fe5ec0f2
Alex Axthelm <aaxthelm@RMI.org>
2024-05-13 16:44:36 +0200

    namespace dependencies

--- R/run_analysis.R
+++ R/run_analysis.R
@@ -1,12 +1,6 @@
 run_analysis <- function(
   cfg_path = commandArgs(trailingOnly = TRUE)
 ) {
-  suppressPackageStartupMessages({
-    library(pacta.portfolio.utils)
-    library(pacta.portfolio.allocate)
-    library(dplyr)
-    library(jsonlite)
-  })
 
   # defaulting to WARN to maintain current (silent) behavior.
   logger::log_threshold(Sys.getenv("LOG_LEVEL", "WARN"))
@@ -29,8 +23,10 @@ run_analysis <- function(
   total_portfolio_path <- file.path(cfg[["output_dir"]], "total_portfolio.rds")
   if (file.exists(total_portfolio_path)) {
     total_portfolio <- readRDS(total_portfolio_path)
-    log_trace("Checking for PACTA relevant data in file: \"{total_portfolio_path}\".")
-    quit_if_no_pacta_relevant_data(total_portfolio)
+    log_trace(
+      "Checking for PACTA relevant data in file: \"{total_portfolio_path}\"."
+    )
+    pacta.portfolio.utils::quit_if_no_pacta_relevant_data(total_portfolio)
   } else {
     log_warn("file \"{total_portfolio_path}\" does not exist.")
     warning("This is weird... the `total_portfolio.rds` file does not exist in the `30_Processed_inputs` directory.")
@@ -42,7 +38,10 @@ run_analysis <- function(
   log_info("Starting equity calculations.")
 
   log_debug("Subsetting equity portfolio.")
-  port_raw_all_eq <- create_portfolio_subset(total_portfolio, "Equity")
+  port_raw_all_eq <- pacta.portfolio.allocate::create_portfolio_subset(
+    portfolio = total_portfolio,
+    portfolio_type = "Equity"
+  )
 
   if (inherits(port_raw_all_eq, "data.frame") && nrow(port_raw_all_eq) > 0L) {
     log_info("Equity portfolio has data. Beginning equity calculations.")
@@ -51,10 +50,13 @@ run_analysis <- function(
     port_all_eq <- NA
 
     log_debug("Calculating quity portfolio weights.")
-    port_eq <- calculate_weights(port_raw_all_eq, "Equity")
+    port_eq <- pacta.portfolio.allocate::calculate_weights(
+      portfolio = port_raw_all_eq,
+      portfolio_type = "Equity"
+    )
 
     log_debug("Merging ABCD data from database into equity portfolio.")
-    port_eq <- merge_abcd_from_db(
+    port_eq <- pacta.portfolio.allocate::merge_abcd_from_db(
       portfolio = port_eq,
       portfolio_type = "Equity",
       db_dir = cfg[["data_dir"]],
@@ -68,59 +70,70 @@ run_analysis <- function(
     # Portfolio weight methodology
     log_info("Calculating portfolio weight methodology.")
     log_debug("Calculating portfolio weight allocation.")
-    port_pw_eq <- port_weight_allocation(port_eq)
+    port_pw_eq <- pacta.portfolio.allocate::port_weight_allocation(port_eq)
 
     log_debug("Aggregating companies for portfolio weight calculation.")
-    company_pw_eq <- aggregate_company(port_pw_eq)
+    company_pw_eq <- pacta.portfolio.allocate::aggregate_company(port_pw_eq)
 
     log_debug("Aggregating portfolio for portfolio weight calculation.")
-    port_pw_eq <- aggregate_portfolio(company_pw_eq)
+    port_pw_eq <- pacta.portfolio.allocate::aggregate_portfolio(company_pw_eq)
 
     # Ownership weight methodology
     log_info("Calculating ownership methodology.")
     log_debug("Calculating ownership allocation.")
-    port_own_eq <- ownership_allocation(port_eq)
+    port_own_eq <- pacta.portfolio.allocate::ownership_allocation(port_eq)
 
     log_debug("Aggregating companies for ownership calculation.")
-    company_own_eq <- aggregate_company(port_own_eq)
+    company_own_eq <- pacta.portfolio.allocate::aggregate_company(port_own_eq)
 
     log_debug("Aggregating portfolio for ownership calculation.")
-    port_own_eq <- aggregate_portfolio(company_own_eq)
+    port_own_eq <- pacta.portfolio.allocate::aggregate_portfolio(company_own_eq)
 
     # Create combined outputs
     log_debug("Creating combined equity company outputs.")
-    company_all_eq <- bind_rows(company_pw_eq, company_own_eq)
+    company_all_eq <- dplyr::bind_rows(company_pw_eq, company_own_eq)
 
     log_debug("Creating combined equity portfolio outputs.")
-    port_all_eq <- bind_rows(port_pw_eq, port_own_eq)
+    port_all_eq <- dplyr::bind_rows(port_pw_eq, port_own_eq)
 
     if (cfg[["has_map"]]) {
       log_debug("Creating equity map outputs.")
-      abcd_raw_eq <- get_abcd_raw("Equity")
+      abcd_raw_eq <- pacta.portfolio.allocate::get_abcd_raw("Equity")
       log_debug("Merging geography data into equity map outputs.")
-      map_eq <- merge_in_geography(company_all_eq, abcd_raw_eq)
+      map_eq <- pacta.portfolio.allocate::merge_in_geography(
+        portfolio = company_all_eq,
+        ald_raw = abcd_raw_eq
+      )
       log_trace("Removing abcd_raw_eq object from memory.")
       rm(abcd_raw_eq)
 
       log_debug("Aggregating equity map data.")
-      map_eq <- aggregate_map_data(map_eq)
+      map_eq <- pacta.portfolio.allocate::aggregate_map_data(map_eq)
     }
 
     # Technology Share Calculation
     log_debug("Calculating equity portfolio technology share.")
-    port_all_eq <- calculate_technology_share(port_all_eq)
+    port_all_eq <- pacta.portfolio.allocate::calculate_technology_share(
+      df = port_all_eq
+    )
 
     log_debug("Calculating equity company technology share.")
-    company_all_eq <- calculate_technology_share(company_all_eq)
+    company_all_eq <- pacta.portfolio.allocate::calculate_technology_share(
+      df = company_all_eq
+    )
 
     # Scenario alignment calculations
     log_debug("Calculating equity portfolio scenario alignment.")
-    port_all_eq <- calculate_scenario_alignment(port_all_eq)
+    port_all_eq <- pacta.portfolio.allocate::calculate_scenario_alignment(
+      df = port_all_eq
+    )
 
     log_debug("Calculating equity company scenario alignment.")
-    company_all_eq <- calculate_scenario_alignment(company_all_eq)
+    company_all_eq <- pacta.portfolio.allocate::calculate_scenario_alignment(
+      df = company_all_eq
+    )
 
-    if (data_check(company_all_eq)) {
+    if (pacta.portfolio.utils::data_check(company_all_eq)) {
       log_debug("Saving equity company results.")
       saveRDS(
         company_all_eq,
@@ -128,7 +141,7 @@ run_analysis <- function(
       )
     }
 
-    if (data_check(port_all_eq)) {
+    if (pacta.portfolio.utils::data_check(port_all_eq)) {
       log_debug("Saving equity portfolio results.")
       saveRDS(
         port_all_eq,
@@ -137,7 +150,7 @@ run_analysis <- function(
     }
 
     if (cfg[["has_map"]]) {
-      if (data_check(map_eq)) {
+      if (pacta.portfolio.utils::data_check(map_eq)) {
         log_debug("Saving equity map results.")
         saveRDS(
           map_eq,
@@ -164,7 +177,10 @@ run_analysis <- function(
   # Bonds -------------------------------------------------------------------
 
   log_info("Starting bonds calculations.")
-  port_raw_all_cb <- create_portfolio_subset(total_portfolio, "Bonds")
+  port_raw_all_cb <- pacta.portfolio.allocate::create_portfolio_subset(
+    portfolio = total_portfolio,
+    portfolio_type = "Bonds"
+  )
 
   if (inherits(port_raw_all_cb, "data.frame") && nrow(port_raw_all_cb) > 0L) {
     log_info("Bonds portfolio has data. Beginning bonds calculations.")
@@ -173,10 +189,13 @@ run_analysis <- function(
     port_all_cb <- NA
 
     log_debug("Calculating bonds portfolio weights.")
-    port_cb <- calculate_weights(port_raw_all_cb, "Bonds")
+    port_cb <- pacta.portfolio.allocate::calculate_weights(
+      portfolio = port_raw_all_cb,
+      portfolio_type = "Bonds"
+    )
 
     log_debug("Merging ABCD data from database into bonds portfolio.")
-    port_cb <- merge_abcd_from_db(
+    port_cb <- pacta.portfolio.allocate::merge_abcd_from_db(
       portfolio = port_cb,
       portfolio_type = "Bonds",
       db_dir = cfg[["data_dir"]],
@@ -190,17 +209,17 @@ run_analysis <- function(
     # Portfolio weight methodology
     log_info("Calculating bonds portfolio weight methodology.")
     log_debug("Calculating bonds portfolio weight allocation.")
-    port_pw_cb <- port_weight_allocation(port_cb)
+    port_pw_cb <- pacta.portfolio.allocate::port_weight_allocation(port_cb)
 
     log_debug(
       "Aggregating companies for bonds portfolio weight calculation."
     )
-    company_pw_cb <- aggregate_company(port_pw_cb)
+    company_pw_cb <- pacta.portfolio.allocate::aggregate_company(port_pw_cb)
 
     log_debug(
       "Aggregating portfolio for bonds portfolio weight calculation."
     )
-    port_pw_cb <- aggregate_portfolio(company_pw_cb)
+    port_pw_cb <- pacta.portfolio.allocate::aggregate_portfolio(company_pw_cb)
 
     # Create combined outputs
     log_debug("Creating combined bonds company outputs.")
@@ -210,38 +229,49 @@ run_analysis <- function(
     port_all_cb <- port_pw_cb
 
     if (cfg[["has_map"]]) {
-      if (data_check(company_all_cb)) {
+      if (pacta.portfolio.utils::data_check(company_all_cb)) {
         log_debug("Creating bonds map outputs.")
-        abcd_raw_cb <- get_abcd_raw("Bonds")
+        abcd_raw_cb <- pacta.portfolio.allocate::get_abcd_raw("Bonds")
         log_debug("Merging geography data into bonds map outputs.")
-        map_cb <- merge_in_geography(company_all_cb, abcd_raw_cb)
+        map_cb <- pacta.portfolio.allocate::merge_in_geography(
+          portfolio = company_all_cb,
+          ald_raw = abcd_raw_cb
+        )
         log_trace("Removing abcd_raw_cb object from memory.")
         rm(abcd_raw_cb)
 
         log_debug("Aggregating bonds map data.")
-        map_cb <- aggregate_map_data(map_cb)
+        map_cb <- pacta.portfolio.allocate::aggregate_map_data(map_cb)
       }
     }
 
     # Technology Share Calculation
     if (nrow(port_all_cb) > 0L) {
       log_debug("Calculating bonds portfolio technology share.")
-      port_all_cb <- calculate_technology_share(port_all_cb)
+      port_all_cb <- pacta.portfolio.allocate::calculate_technology_share(
+        df = port_all_cb
+      )
     }
 
     if (nrow(company_all_cb) > 0L) {
       log_debug("Calculating bonds company technology share.")
-      company_all_cb <- calculate_technology_share(company_all_cb)
+      company_all_cb <- pacta.portfolio.allocate::calculate_technology_share(
+        df = company_all_cb
+      )
     }
 
     # Scenario alignment calculations
     log_debug("Calculating bonds portfolio scenario alignment.")
-    port_all_cb <- calculate_scenario_alignment(port_all_cb)
+    port_all_cb <- pacta.portfolio.allocate::calculate_scenario_alignment(
+      df = port_all_cb
+    )
 
     log_debug("Calculating bonds company scenario alignment.")
-    company_all_cb <- calculate_scenario_alignment(company_all_cb)
+    company_all_cb <- pacta.portfolio.allocate::calculate_scenario_alignment(
+      df = company_all_cb
+    )
 
-    if (data_check(company_all_cb)) {
+    if (pacta.portfolio.utils::data_check(company_all_cb)) {
       log_debug("Saving bonds company results.")
       saveRDS(
         company_all_cb,
@@ -249,7 +279,7 @@ run_analysis <- function(
       )
     }
 
-    if (data_check(port_all_cb)) {
+    if (pacta.portfolio.utils::data_check(port_all_cb)) {
       log_debug("Saving bonds portfolio results.")
       saveRDS(
         port_all_cb,
@@ -258,7 +288,7 @@ run_analysis <- function(
     }
 
     if (cfg[["has_map"]]) {
-      if (data_check(map_cb)) {
+      if (pacta.portfolio.utils::data_check(map_cb)) {
         log_debug("Saving bonds map results.")
         saveRDS(map_cb, file.path(cfg[["output_dir"]], "Bonds_results_map.rds"))
       }
