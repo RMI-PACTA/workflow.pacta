fd9914ba3df139f4adb85993c7b5d19b7344cd56
Alex Axthelm <aaxthelm@RMI.org>
2024-05-20 17:08:19 +0200

    rename from `cfg` to `params`

--- R/run_pacta.R
+++ R/run_pacta.R
@@ -1,30 +1,30 @@
 run_pacta <- function(
-  cfg_path = commandArgs(trailingOnly = TRUE)
+  raw_params = commandArgs(trailingOnly = TRUE)
 ) {
   log_info("Running PACTA")
 
   # Read Params
   log_trace("Determining configuration file path")
-  if (length(cfg_path) == 0L || cfg_path == "") {
+  if (length(raw_params) == 0L || raw_params == "") {
     log_warn("No configuration file specified, using default")
-    cfg_path <- file.path("input_dir", "default_config.json")
+    raw_params <- file.path("input_dir", "default_config.json")
   }
-  log_debug("Loading configuration from file: \"{cfg_path}\".")
-  cfg <- jsonlite::fromJSON(cfg_path)
+  log_debug("Loading configuration from file: \"{params_path}\".")
+  params <- jsonlite::fromJSON(raw_params)
 
   run_audit(
-    data_dir = cfg[["data_dir"]],
-    portfolio_path = cfg[["portfolio_path"]],
-    output_dir = cfg[["output_dir"]]
+    data_dir = params[["data_dir"]],
+    portfolio_path = params[["portfolio_path"]],
+    output_dir = params[["output_dir"]]
   )
   run_analysis(
-    data_dir = cfg[["data_dir"]],
-    output_dir = cfg[["output_dir"]],
-    equity_market_list = cfg[["equity_market_list"]],
-    scenario_sources_list = cfg[["scenario_sources_list"]],
-    scenario_geographies_list = cfg[["scenario_geographies_list"]],
-    sector_list = cfg[["sector_list"]],
-    has_map = cfg[["has_map"]]
+    data_dir = params[["data_dir"]],
+    output_dir = params[["output_dir"]],
+    equity_market_list = params[["equity_market_list"]],
+    scenario_sources_list = params[["scenario_sources_list"]],
+    scenario_geographies_list = params[["scenario_geographies_list"]],
+    sector_list = params[["sector_list"]],
+    has_map = params[["has_map"]]
   )
   log_info("PACTA run complete.")
 }
