7cbf522ac21f1f484fddf154e8364d102719794c
Alex Axthelm <aaxthelm@RMI.org>
2024-05-27 20:07:37 +0200

    Add validation for raw params (before inherit)

--- R/run_pacta.R
+++ R/run_pacta.R
@@ -4,6 +4,7 @@ run_pacta <- function(
   output_dir = Sys.getenv("OUTPUT_DIR"),
   portfolio_dir = Sys.getenv("PORTFOLIO_DIR")
 ) {
+
   log_debug("Checking configuration.")
   if (is.null(pacta_data_dir) || pacta_data_dir == "") {
     log_error("PACTA_DATA_DIR not set.")
@@ -22,8 +23,30 @@ run_pacta <- function(
   # Read Params
   log_trace("Processing input parameters.")
   if (length(raw_params) == 0L || all(raw_params == "")) {
-    log_error("No configuration file specified.")
+    log_error("No parameters specified.")
   }
+
+  log_trace("Validating raw input parameters.")
+  raw_input_validation_results <- jsonvalidate::json_validate(
+    json = raw_params,
+    schema = system.file(
+      "extdata", "schema", "rawParameters.json",
+      package = "workflow.pacta"
+    ),
+    verbose = TRUE,
+    greedy = FALSE,
+    engine = "ajv"
+  )
+  if (raw_input_validation_results) {
+    log_trace("Raw input parameters are valid.")
+  } else {
+    log_error(
+      "Invalid raw input parameters. ",
+      "Must include \"inherit\" key, or match full schema."
+    )
+    stop("Invalid raw input parameters.")
+  }
+
   params <- pacta.workflow.utils:::parse_params(
     json = raw_params,
     inheritence_search_paths = system.file(
