5470dc3cd8a8ec74153e93d6ccc6e328c79021ce
Alex Axthelm <aaxthelm@RMI.org>
2024-08-06 17:32:43 +0200

    feat(app): #77 move param parsing to script instead of function

--- R/run_pacta.R
+++ R/run_pacta.R
@@ -1,5 +1,5 @@
 run_pacta <- function(
-  raw_params = commandArgs(trailingOnly = TRUE),
+  params,
   pacta_data_dir = Sys.getenv("PACTA_DATA_DIR"),
   output_dir = Sys.getenv("ANALYSIS_OUTPUT_DIR"),
   portfolio_dir = Sys.getenv("PORTFOLIO_DIR")
@@ -19,45 +19,6 @@ run_pacta <- function(
     stop("PORTFOLIO_DIR not set.")
   }
   log_info("Running PACTA")
-
-  # Read Params
-  log_trace("Processing input parameters.")
-  if (length(raw_params) == 0L || all(raw_params == "")) {
-    log_error("No parameters specified.")
-  }
-
-  log_trace("Validating raw input parameters.")
-  raw_input_validation_results <- jsonvalidate::json_validate(
-    json = raw_params,
-    schema = system.file(
-      "extdata", "schema", "rawParameters.json",
-      package = "workflow.pacta"
-    ),
-    verbose = TRUE,
-    greedy = FALSE,
-    engine = "ajv"
-  )
-  if (raw_input_validation_results) {
-    log_trace("Raw input parameters are valid.")
-  } else {
-    log_error(
-      "Invalid raw input parameters. ",
-      "Must include \"inherit\" key, or match full schema."
-    )
-    stop("Invalid raw input parameters.")
-  }
-
-  params <- pacta.workflow.utils:::parse_params(
-    json = raw_params,
-    inheritence_search_paths = system.file(
-      "extdata", "parameters",
-      package = "workflow.pacta"
-    ),
-    schema_file = system.file(
-      "extdata", "schema", "portfolioParameters.json",
-      package = "workflow.pacta"
-    )
-  )
 
   run_audit(
     portfolio_files = params[["portfolio"]][["files"]],
--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -1,2 +1,17 @@
 logger::log_threshold(Sys.getenv("LOG_LEVEL", "INFO"))
+params <- pacta.workflow.utils::parse_raw_params(
+  json = commandArgs(trailingOnly = TRUE),
+  inheritence_search_paths = system.file(
+    "extdata", "parameters",
+    package = "workflow.pacta"
+  ),
+  schema_file = system.file(
+    "extdata", "schema", "portfolioParameters.json",
+    package = "workflow.pacta"
+  ),
+  raw_schema_file = system.file(
+    "extdata", "schema", "rawParameters.json",
+    package = "workflow.pacta"
+  )
+)
 workflow.pacta:::run_pacta(commandArgs(trailingOnly = TRUE))
