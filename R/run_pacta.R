dd23b76475d24457ca1296912f8e98541fb3b3ae
Alex Axthelm <aaxthelm@RMI.org>
2024-08-13 15:13:50 +0200

    Add prechecks for pacta full function

--- R/run_pacta.R
+++ R/run_pacta.R
@@ -59,6 +59,18 @@ run_pacta <- function(
     )
   )
 
+  audit_prechecks(
+    portfolio_files = params[["portfolio"]][["files"]],
+    pacta_data_dir = pacta_data_dir,
+    portfolio_dir = portfolio_dir,
+    output_dir = output_dir
+  )
+  analysis_prechecks(
+    total_portfolio_path = total_portfolio_path,
+    pacta_data_dir = pacta_data_dir,
+    output_dir = output_dir
+  )
+
   run_audit(
     portfolio_files = params[["portfolio"]][["files"]],
     pacta_data_dir = pacta_data_dir,
